afeae2d58a9b55c5fc7077db124be51cc6dc2764
ZBXImface <2297823952@qq.com>
2020-02-17 21:26:07 +0800

    配置项目基础依赖

---
+++ src/main/java/com/gree/ApplicationMain.java
@@ -0,0 +1,12 @@
+package com.gree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMain {
+    public static void main(String[] args){
+        System.out.println("启动项目");
+        SpringApplication.run(ApplicationMain.class,args);
+    }
+}
