30231d91fef9283d044f33756780fd0318064ac9
ZBXImface <2297823952@qq.com>
2020-03-20 23:07:55 +0800

    新增本地服务类

---
+++ src/main/java/com/gree/common/utilis/ServerConstant.java
@@ -0,0 +1,10 @@
+package com.gree.common.utilis;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerConstant {
+    /*定义用于记录用户操作记录*/
+    public static Map<String,Object> userRequestSession = new HashMap<>();
+    public static Map<String,Object> serverCommondSession = new HashMap<>();
+}
