d1904d56dbbe783e2efe43abaa14f7218a5e8289
ZBXImface <2297823952@qq.com>
2020-12-25 21:24:41 +0800

    新增订单管理功能接口

---
+++ src/main/java/com/gree/book/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.gree.book.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.sun.media.sound.JARSoundbankReader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @RequestMapping("/getOrderDetailsById")
+    public JSONObject getOrderDetailsById(){
+        return null;
+    }
+}
